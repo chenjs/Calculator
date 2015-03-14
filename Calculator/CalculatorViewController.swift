8103da09a937c54c6d83e6f1ea04b1994fc5fbab
chenjs <chenjs.5492@gmail.com>
2015-03-14 22:41:28 +0800

    1. Add operations(times, divide, plus, minus, sqrt) 2. Autolayout buttons.

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -27,9 +27,40 @@ class CalculatorViewController: UIViewController
         }
     }
     
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        let operation = sender.currentTitle!
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default:break
+        }
+        
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     var operandStack = Array<Double>()
     
-    @IBAction func enter(sender: AnyObject) {
+    @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
         println("operandStack = \(operandStack)")
